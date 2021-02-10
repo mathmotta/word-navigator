22b1674bf5e2b9c75b3e79a1254d8f27767733de
mathewsmotta <mathewscsmotta@gmail.com>
2021-02-10 23:37:30 +0000

    Setup DI and Logging

---
+++ src/BluePrism.WordNavigator.Bootstrap/Entrypoint/EntrypointService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BluePrism.WordNavigator.Bootstrap
+{
+    public class EntrypointService : IEntrypointService
+    {
+        private readonly ILogger<EntrypointService> _log;
+        private readonly IConfiguration _config;
+
+        public EntrypointService(ILogger<EntrypointService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Execute(string[] args)
+        {
+
+        }
+    }
+}
