55e167076122eafb4d81f96bf4cd865a74a18a63
mathewsmotta <mathewscsmotta@gmail.com>
2021-02-11 22:32:16 +0000

    Added parsing to hash set and concurrent hash set for async enumerable

---
+++ tests/BluePrism.WordNavigator.Common.Tests/Extensions/AsyncEnumerableExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using BluePrism.WordNavigator.Common.Concurrent;
+using BluePrism.WordNavigator.Common.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluePrism.WordNavigator.Common.Tests.Extensions
+{
+    public class AsyncEnumerableExtensionsTests
+    {
+       [Test]
+       public async Task ToConcurrentHashSet_Success()
+        {
+            var list = new List<int>() { 1 };
+            var asyncEnumerable = list.ToAsyncEnumerable();
+
+            var concurrentHashSet = await asyncEnumerable.ToConcurrentHashSet();
+            Assert.IsTrue(concurrentHashSet.Contains(1));
+        }
+
+        [Test]
+        public async Task ToHashSet_Success()
+        {
+            var list = new List<int>() { 1 };
+            var asyncEnumerable = list.ToAsyncEnumerable();
+
+            var hashSet = await asyncEnumerable.ToHashSet();
+            Assert.IsTrue(hashSet.Contains(1));
+        }
+    }
+}
