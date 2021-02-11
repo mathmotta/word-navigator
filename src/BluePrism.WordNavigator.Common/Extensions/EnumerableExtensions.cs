942b41045c9a33a4eeac5992ab1487762063d6ee
mathewsmotta <mathewscsmotta@gmail.com>
2021-02-11 22:21:37 +0000

    Added enumerable extensions to transfor to ConcurrentHashSet

---
+++ src/BluePrism.WordNavigator.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using BluePrism.WordNavigator.Common.Concurrent;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluePrism.WordNavigator.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static ConcurrentHashSet<TSource> ToConcurrentHashSet<TSource>(this IEnumerable<TSource> source)
+        {
+            var hashSet = new ConcurrentHashSet<TSource>();
+            foreach (TSource element in source)
+            {
+                hashSet.Add(element);
+            }
+            return hashSet;
+        }
+    }
+}
---
+++ tests/BluePrism.WordNavigator.Common.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BluePrism.WordNavigator.Common.Extensions;
+
+
+namespace BluePrism.WordNavigator.Common.Tests.Extensions
+{
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void ToConcurrentHashSet_Success()
+        {
+            var enumerable = new List<int>();
+            enumerable.Add(1);
+            var result = enumerable.ToConcurrentHashSet();
+
+            Assert.IsTrue(result.Contains(1));
+        }
+    }
+}
