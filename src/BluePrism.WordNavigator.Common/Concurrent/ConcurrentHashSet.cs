c27617b1cc3cb664b29f7abb5109c6a3336cb21e
mathewsmotta <mathewscsmotta@gmail.com>
2021-02-11 22:14:03 +0000

    Created a Concurrent version of HashSet for usage in the navigator

---
+++ src/BluePrism.WordNavigator.Common/Concurrent/ConcurrentHashSet.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading;
+
+namespace BluePrism.WordNavigator.Common.Concurrent
+{
+    public class ConcurrentHashSet<T> : IDisposable, IEnumerable<T>, IDeserializationCallback, ISerializable
+    {
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        private readonly HashSet<T> _hashSet = new HashSet<T>();
+
+        public bool Add(T item)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                return _hashSet.Add(item);
+            }
+            finally
+            {
+                if (_lock.IsWriteLockHeld) _lock.ExitWriteLock();
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                return _hashSet.Remove(item);
+            }
+            finally
+            {
+                if (_lock.IsWriteLockHeld) _lock.ExitWriteLock();
+            }
+        }
+
+
+        public int RemoveWhere(Predicate<T> match)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                return _hashSet.RemoveWhere(match);
+            }
+            finally
+            {
+                if (_lock.IsWriteLockHeld) _lock.ExitWriteLock();
+            }
+        }
+
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                return _hashSet.GetEnumerator();
+            }
+            finally
+            {
+                if (_lock.IsWriteLockHeld) _lock.ExitWriteLock();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                return _hashSet.GetEnumerator();
+            }
+            finally
+            {
+                if (_lock.IsWriteLockHeld) _lock.ExitWriteLock();
+            }
+        }
+
+        public void Clear()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _hashSet.Clear();
+            }
+            finally
+            {
+                if (_lock.IsWriteLockHeld) _lock.ExitWriteLock();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _hashSet.Contains(item);
+            }
+            finally
+            {
+                if (_lock.IsReadLockHeld) _lock.ExitReadLock();
+            }
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                _lock.EnterReadLock();
+                try
+                {
+                    return _hashSet.Count;
+                }
+                finally
+                {
+                    if (_lock.IsReadLockHeld) _lock.ExitReadLock();
+                }
+            }
+        }
+
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                _hashSet.GetObjectData(info, context);
+            }
+            finally
+            {
+                if (_lock.IsReadLockHeld) _lock.ExitReadLock();
+            }
+        }
+
+        public void OnDeserialization(object? sender)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                _hashSet.OnDeserialization(sender);
+            }
+            finally
+            {
+                if (_lock.IsReadLockHeld) _lock.ExitReadLock();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                if (_lock != null)
+                    _lock.Dispose();
+        }
+
+
+
+        ~ConcurrentHashSet()
+        {
+            Dispose(false);
+        }
+    }
+}
