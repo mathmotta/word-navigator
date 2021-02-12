d86232cd226338c255759992785f713f16a87950
mathewsmotta <mathewscsmotta@gmail.com>
2021-02-12 16:26:34 +0000

    Added documentation

---
+++ src/BluePrism.WordNavigator.Core/Navigation/IWordNavigationService.cs
@@ -0,0 +1,39 @@
+﻿using BluePrism.WordNavigator.Common.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BluePrism.WordNavigator.Core
+{
+    /// <summary>
+    /// <para>Exposes the default Word Navigation Service which specifies functionality to seek and find a <b>target</b> string from phased modifications of a <b>start</b> string.</para>
+    /// <para>Each phase may only change one char in the string, creating a similarity, and such similarity must exist in the provided <b>source</b></para>
+    /// <para>E.g. start is 'same', target is 'cost', should result in all shortest paths, one of them being [same -> came -> case -> cast -> cost]</para>
+    /// </summary>
+    public interface IWordNavigationService
+    {
+        /// <summary>
+        /// Seeks for a given target string, running from a given start string. All similarities must be part of the given source.
+        /// </summary>
+        /// <param name="start">The start string</param>
+        /// <param name="target">The target string</param>
+        /// <param name="source">A concurrent hash set source, where all strings should be present</param>
+        /// <returns>A collection of shortest paths.</returns>
+        Task<ICollection<ICollection<string>>> Seek(string start, string target, ConcurrentHashSet<string> source);
+        /// <summary>
+        /// Seeks for a given target string, running from a given start string. All similarities must be part of the given source.
+        /// </summary>
+        /// <param name="start">The start string</param>
+        /// <param name="target">The target string</param>
+        /// <param name="source">An asynchronous enumerable source, where all strings should be present</param>
+        /// <returns>A collection of shortest paths.</returns>
+        Task<ICollection<ICollection<string>>> Seek(string start, string target, IAsyncEnumerable<string> source);
+        /// <summary>
+        /// Seeks for a given target string, running from a given start string. All similarities must be part of the given source.
+        /// </summary>
+        /// <param name="start">The start string</param>
+        /// <param name="target">The target string</param>
+        /// <param name="source">An enumerable source, where all strings should be present</param>
+        /// <returns>A collection of shortest paths.</returns>
+        Task<ICollection<ICollection<string>>> Seek(string start, string target, IEnumerable<string> source);
+    }
+}
--- src/BluePrism.WordNavigator.Core/Navigation/WordNavigationService.cs
+++ src/BluePrism.WordNavigator.Core/Navigation/WordNavigationService.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BluePrism.WordNavigator.Core
 {
-    public class WordNavigationService
+    public class WordNavigationService : IWordNavigationService
     {
         private ILogger<WordNavigationService> _log;
 
@@ -21,47 +21,61 @@ namespace BluePrism.WordNavigator.Core
         public virtual async Task<ICollection<ICollection<string>>> Seek(string start, string target, IEnumerable<string> source)
         {
             ConcurrentHashSet<string> notKnown = source.ToConcurrentHashSet();
-            notKnown.Remove(start);
-
             return await Seek(start, target, notKnown);
         }
 
         public virtual async Task<ICollection<ICollection<string>>> Seek(string start, string target, IAsyncEnumerable<string> source)
         {
             ConcurrentHashSet<string> notKnown = await source.ToConcurrentHashSet();
-            notKnown.Remove(start);
-
             return await Seek(start, target, notKnown);
         }
 
-        public virtual async Task<ICollection<ICollection<string>>> Seek(string start, string target, ConcurrentHashSet<string> notKnown)
+        public virtual async Task<ICollection<ICollection<string>>> Seek(string start, string target, ConcurrentHashSet<string> source)
         {
+            source.Remove(start);
+
+            _log.LogDebug("Started seeking with start {start} and target {target}.", start, target);
             var similarityGroups = new ConcurrentDictionary<string, ICollection<string>>();
-            bool foundTarget = await SeekTarget(start, target, notKnown, similarityGroups);
+            bool foundTarget = await SeekTarget(start, target, source, similarityGroups);
             var researchResult = new List<ICollection<string>>();
             if (!foundTarget)
+            {
+                _log.LogDebug("Target was not found. No results to be shown.");
                 return researchResult;
+            }
+            _log.LogDebug("Target found. Getting all paths next.");
             var paths = new List<string> { start };
             SeekAllShortestPaths(start, target, similarityGroups.ToDictionary(kv => kv.Key, kv => kv.Value.ToList()), researchResult, paths);
-
+            _log.LogDebug("Finalized seek.");
+            source.Dispose();
             return researchResult;
         }
 
-        public virtual async Task<bool> SeekTarget(string start, string target, ConcurrentHashSet<string> notKnown, ConcurrentDictionary<string, ICollection<string>> similarityGroups)
+        /// <summary>
+        /// Seeks a target string, starting from a start string, with a given grouped similarities that must be part of a given source
+        /// </summary>
+        /// <param name="start">The start string to search from</param>
+        /// <param name="target">The target string to find</param>
+        /// <param name="source">The allowed contents that the search can be done at</param>
+        /// <param name="similarityGroups">The similarity groups that were already found</param>
+        /// <returns>True if the target is found</returns>
+        public virtual async Task<bool> SeekTarget(string start, string target, ConcurrentHashSet<string> source, ConcurrentDictionary<string, ICollection<string>> similarityGroups)
         {
             var pathsContent = new List<string>();
             pathsContent.Add(start);
             var foundTarget = false;
             while (pathsContent.Any() && !foundTarget)
             {
-                var alreadyKnown = new ConcurrentHashSet<String>();
+                var alreadyKnown = new ConcurrentHashSet<string>();
                 var tasks = new List<Task<bool>>();
                 var length = pathsContent.Count;
+                _log.LogDebug("Iterating all paths seeking for target. Size is {length}", length);
                 for (int i = 0; i < length; i++)
                 {
                     var group = pathsContent.First();
                     pathsContent.Remove(group);
-                    tasks.Add(Task.Run(() => IterateLetters(group, target, notKnown, similarityGroups, alreadyKnown, pathsContent)));
+
+                    tasks.Add(Task.Run(() => IterateLetters(group, target, source, similarityGroups, alreadyKnown, pathsContent)));
                 }
                 bool[] foundInSeries = await Task.WhenAll(tasks);
                 if (foundInSeries.Any(f => f))
@@ -69,48 +83,86 @@ namespace BluePrism.WordNavigator.Core
                     foundTarget = true;
                 }
 
-                notKnown.RemoveWhere(alreadyKnown.Contains);
+                source.RemoveWhere(alreadyKnown.Contains);
             }
             return foundTarget;
         }
 
-        public virtual bool IterateLetters(string group, string target, ConcurrentHashSet<string> notKnown, ConcurrentDictionary<string, ICollection<string>> similarityGroups, ConcurrentHashSet<string> alreadyKnown, ICollection<string> pathsContent)
+        /// <summary>
+        /// Iterate over the letters of a given word to find similarities
+        /// </summary>
+        /// <param name="group">The group where the similarity will belong to</param>
+        /// <param name="target">The target word to be found</param>
+        /// <param name="source">The allowed contents that the search can be done at</param>
+        /// <param name="similarityGroups">The similarity groups that were already found</param>
+        /// <param name="alreadyKnown">The already found words</param>
+        /// <param name="pathsContent">List of valid paths</param>
+        /// <returns>True if the target is found</returns>
+        public virtual bool IterateLetters(string group, string target, ConcurrentHashSet<string> source, ConcurrentDictionary<string, ICollection<string>> similarityGroups, ConcurrentHashSet<string> alreadyKnown, ICollection<string> pathsContent)
         {
             if (group == null)
+            {
+                _log.LogDebug("Group is null, skipping word.");
                 return false;
+            }
+            _log.LogDebug("Iterating letters in word: {group}", group);
             var found = false;
             for (int l = 0; l < group.Length; l++)
             {
-                bool foundInCurrent = FindAndMatchSimilarities(l, group, target, notKnown, similarityGroups, alreadyKnown, pathsContent);
+                _log.LogDebug("Finding similarities for word {group} changing letter at position {l}", group, l);
+                bool foundInCurrent = FindAndMatchSimilarities(l, group, target, source, similarityGroups, alreadyKnown, pathsContent);
                 if (foundInCurrent)
                 {
+                    _log.LogDebug("Target was found!");
                     found = true;
                 }
             }
             return found;
         }
 
-        public virtual bool FindAndMatchSimilarities(int index, string group, string target, ConcurrentHashSet<string> notKnown, ConcurrentDictionary<string, ICollection<string>> similarityGroups, ConcurrentHashSet<string> alreadyKnown, ICollection<string> pathsContent)
+        /// <summary>
+        /// Match a similarity with a group, if applicable
+        /// </summary>
+        /// <param name="index">The index letter to change a word</param>
+        /// <param name="group">The group where similarities will be added to</param>
+        /// <param name="target">The target word to be found</param>
+        /// <param name="source">The allowed contents that the search can be done at</param>
+        /// <param name="similarityGroups">The similarity groups that were already found</param>
+        /// <param name="alreadyKnown">The already found words</param>
+        /// <param name="pathsContent">List of valid paths</param>
+        /// <returns>True if target is found</returns>
+        public virtual bool FindAndMatchSimilarities(int index, string group, string target, ConcurrentHashSet<string> source, ConcurrentDictionary<string, ICollection<string>> similarityGroups, ConcurrentHashSet<string> alreadyKnown, ICollection<string> pathsContent)
         {
             var targetFound = false;
+
             for (char k = 'a'; k <= 'z'; k++)
             {
                 string similarity = SubstituteLetter(group, index, k);
-                if (!notKnown.Contains(similarity))
+                if (!source.Contains(similarity))
                 {
+                    _log.LogDebug("Similarity {similarity} does not exist in source, skipping.", similarity);
                     continue;
                 }
+                _log.LogDebug("Adding similarity {similarity} to the list of known elements.", similarity);
                 AddSimilarityToGroup(group, similarity, similarityGroups);
                 pathsContent.AddIf(similarity, !alreadyKnown.Contains(similarity));
                 alreadyKnown.Add(similarity);
                 if (similarity.Equals(target))
                 {
+                    _log.LogInformation("Target {target} was found from similarity of {group}!", target, group);
                     targetFound = true;
                 }
             }
             return targetFound;
         }
 
+        /// <summary>
+        /// Substitue a letter in a string.
+        /// </summary>
+        /// <param name="str">The string to be modified</param>
+        /// <param name="indexToChange">The index of the string to be substituted</param>
+        /// <param name="letter">The letter to be used in the substitution</param>
+        /// <returns>A string similar to the given one, with a single changed letter</returns>
         public virtual string SubstituteLetter(string str, int indexToChange, char letter)
         {
             var chars = str.ToCharArray();
@@ -118,6 +170,13 @@ namespace BluePrism.WordNavigator.Core
             return new string(chars);
         }
 
+        /// <summary>
+        /// Adds a similarity to a group
+        /// </summary>
+        /// <typeparam name="T">The type that is been worked with</typeparam>
+        /// <param name="group">The group to add a similarity to</param>
+        /// <param name="similarity">The similarity to be added</param>
+        /// <param name="similarityGroups">The similarity groups that were already found</param>
         public virtual void AddSimilarityToGroup<T>(T group, T similarity, ConcurrentDictionary<T, ICollection<T>> similarityGroups)
         {
             ICollection<T> similarities = similarityGroups.GetValueOrDefault(group, new List<T>());
@@ -125,6 +184,14 @@ namespace BluePrism.WordNavigator.Core
             similarityGroups.TryAddIf(group, similarities, !similarityGroups.ContainsKey(group));
         }
 
+        /// <summary>
+        /// Recursivelly finds all shortest paths from a list of groups and its similarities
+        /// </summary>
+        /// <param name="start">The start word</param>
+        /// <param name="target">The word to be found</param>
+        /// <param name="similarityGroups">The similarity groups to find the shortest path from</param>
+        /// <param name="researchResult">The result to write to</param>
+        /// <param name="paths">The constructed paths to be added to the result</param>
         public virtual void SeekAllShortestPaths(string start, string target, Dictionary<string, List<string>> similarityGroups, ICollection<ICollection<string>> researchResult, ICollection<string> paths)
         {
             if (start.Equals(target))
